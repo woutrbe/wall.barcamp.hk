2b57a3fe870649f2fc8be016eb16af0d6a44f9a4
Wouter <wouter@woutr.me>
2014-10-17 20:16:57 +0800

    [UPDATE] (single job) Updated safe link for single job

--- client/scripts/controllers/jobsDetailController.js
+++ client/scripts/controllers/jobsDetailController.js
@@ -5,6 +5,9 @@
 		// Redirect to 404 if the job doesn't exist
 		if(job === undefined || job === 'null') $location.path('/404');
 
+		// Set a safe link for the category
+		job.cat.safeLink = job.cat.link.toLowerCase().replace(/ /g, '-');
+
 		$scope.job = job;
 		
 		return $scope;
